b5ece17eb6976ece6b114349a85a8cb3d0cbdc2b
Nile Ravenell <43184722+nileravenell@users.noreply.github.com>
2020-12-03 20:05:24 -0500

    Add files via upload

---
+++ CompleteStartedApplication/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String message;
+        System.out.println("Welcome! How's the weather today? ");
+        message = input.nextLine();
+    }
+}
